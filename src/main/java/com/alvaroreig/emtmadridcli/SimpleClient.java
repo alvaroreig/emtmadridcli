972558c64fd61e7e076a3efe25d385e8bf25bdfb
alvaroreig <alvaroreig@gmail.com>
2014-10-25 14:37:13 +0200

    first maven stub

---
+++ src/main/java/com/alvaroreig/emtmadridcli/SimpleClient.java
@@ -0,0 +1,13 @@
+package com.alvaroreig.emtmadridcli;
+
+/**
+ * Hello world!
+ *
+ */
+public class SimpleClient 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Client Working" );
+    }
+}
